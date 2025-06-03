a6e3d874103913a890b6b3b64c921ee625d32d01
marcelodeus98 <marcelodeus98@gmail.com>
2025-06-03 00:44:06 -0300

    refactor(API): include config vercel for routes

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express, { Handler } from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.enableCors({
     origin: '*',
@@ -15,12 +19,12 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true, 
-      transform: true, 
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
-    const config = new DocumentBuilder()
+  const config = new DocumentBuilder()
     .setTitle('Education API')
     .setDescription('Documentação da API da aplicação de educação')
     .setVersion('1.0')
@@ -32,10 +36,13 @@ async function bootstrap() {
     customCssUrl: 'https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.css',
     customJs: [
       'https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-bundle.js',
-      'https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-standalone-preset.js'
-    ]
+      'https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-standalone-preset.js',
+    ],
   });
 
-  await app.listen(3000);
+  await app.init();
 }
+
 bootstrap();
+
+export const handler: Handler = server;
